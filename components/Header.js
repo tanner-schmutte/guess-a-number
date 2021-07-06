5b48f7867d1f0799f47f6d86ba65150a1ec4b071
Tanner Schmutte <tannerschmutte@gmail.com>
2021-07-05 17:08:38 -0700

    add StartGameScreen screen and Card component

--- components/Header.js
+++ components/Header.js
@@ -18,7 +18,7 @@ const styles = StyleSheet.create({
         width: '100%',
         height: 90,
         paddingTop: 36,
-        backgroundColor: '#f7287b',
+        backgroundColor: '#21b786',
         alignItems: 'center',
         justifyContent: 'center',
     },
