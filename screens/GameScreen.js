58c36c78e5aaf768fb908466c722ab56670af567
Tanner Schmutte <tannerschmutte@gmail.com>
2021-07-07 19:59:21 -0700

    add GameOverScreen

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { View, Text, StyleSheet, Button, Alert } from 'react-native';
 
 import NumberContainer from '../components/NumberContainer';
@@ -19,9 +19,18 @@ const GameScreen = (props) => {
     const [currentGuess, setCurrentGuess] = useState(
         generateRandomBetween(1, 100, props.userChoice)
     );
+    const [rounds, setRounds] = useState(0);
 
     const currentLow = useRef(1);
     const currentHigh = useRef(100);
+
+    const { userChoice, onGameOver } = props;
+
+    useEffect(() => {
+        if (currentGuess === userChoice) {
+            onGameOver(rounds);
+        }
+    }, [currentGuess, userChoice, onGameOver]);
 
     const nextGuessHandler = (direction) => {
         if (
@@ -45,6 +54,7 @@ const GameScreen = (props) => {
         );
 
         setCurrentGuess(nextNumber);
+        setRounds((curRounds) => curRounds + 1);
     };
 
     return (
